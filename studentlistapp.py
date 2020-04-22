186a4444807061274c58fd2f7babd13e956aaeb5
Erishanom <33721329+Erishanom@users.noreply.github.com>
2020-04-22 09:31:18 +0200

    Update
    Lägg till kommentarer till koden

--- studentlistapp.py
+++ studentlistapp.py
@@ -1,4 +1,4 @@
-studentlist = ["johson", "xxyy0011", "joso1122"]
+studentlist = ["johson", "xxyy0011", "joso1122", "Erica"]
 
 print("These students have finished the lab: ")
 for student in studentlist:
